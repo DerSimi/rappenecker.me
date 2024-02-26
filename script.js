b20c8872053c1a73d98f704f114f56c9023048c4
Simon Rappenecker <simi2657@gmail.com>
2024-02-26 11:47:30 +0100

    Added a margin for link, removed imprint.html and created javascript handler for it

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    const imprintElement = document.getElementById("imprint");
+    const contentTextElement = document.querySelector(".box-content-text");
+    const defaultText = contentTextElement.innerHTML;
+
+    imprintElement.addEventListener("click", () => {
+        if (imprintElement.innerHTML === "Imprint") {
+            contentTextElement.innerHTML = `
+                <p>Imprint §5 TGM</p>
+                <br>
+                <p>Address</p>
+                Simon Rappenecker <br>
+                Fichtenweg 7 <br>
+                72076 Tübingen <br>
+                <p>Contact</p>
+                E-Mail: simon@rappenecker.me
+            `;
+            imprintElement.innerHTML = "Home";
+        } else {
+            contentTextElement.innerHTML = defaultText;
+            imprintElement.innerHTML = "Imprint";
+        }
+
+        set = !set;
+    });
+  }
